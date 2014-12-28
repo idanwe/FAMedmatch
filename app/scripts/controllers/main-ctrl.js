a9beae8e80f5bea516b327fa772fc56f57ffdc9c
Idan Wender <idanwe2@gmail.com>
2014-12-28 21:30:34 +0200

    Add header footer layout
    * Add start index (current month)
    * Show the current year at the header
    * Add stylesheets

--- app/scripts/controllers/main-ctrl.js
+++ app/scripts/controllers/main-ctrl.js
@@ -10,13 +10,32 @@
 angular.module('medMatchApp')
   .controller('MainCtrl', function ($scope, $famous, MonthsBuffer) {
     var EventHandler = $famous['famous/core/EventHandler'];
-    $scope.scrollViewEventHandler = new EventHandler();
+
+    $scope.layoutOptions = {
+      headerSize: 44,
+      footerSize: 60
+    };
 
     $scope.monthGridOptions = {
       dimensions: [1, 6]
     };
 
+    $scope.surfaceEventHandler = new EventHandler();
+    $scope.scrollViewEventHandler = new EventHandler();
+
+    $scope.scrollViewEventHandler.on('pageChange', function (options) {
+      $scope.$apply(function() {
+        var currentMonth = $scope.months[options.index];
+        $scope.currentYear = currentMonth.year;
+      });
+    });
+
+
+    // Init
     $scope.months = new MonthsBuffer();
+
+    $scope.startIndex = $scope.months.length / 2;
+    $scope.currentYear = $scope.months[$scope.startIndex].year;
 
     // should move to mmWeek
     $scope.weekGridOptions = {
